c8693d16f62d42e627d66c074b8d9b5839660107
Aaron Utterback <aar0nu@icloud.com>
2019-10-01 18:54:31 -0700

    Add project files.

---
+++ Bookmi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bookmi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
