1d1ad4f46580433957f992e0aa5d0b904c143ce5
aau19 <aau19@DESKTOP-M0K5TMI>
2019-10-21 18:32:50 -0700

    Created Services Class

---
+++ Bookmi/Models/Services.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookmi.Models
+{
+    public class Services
+    {
+        public Services()
+        {
+
+        }
+
+        public string ServiceProvided { get; set; }
+
+        public string ServiceName { get; set; }
+
+        public string ServiceDescription { get; set; }
+
+        public double ServiceCost { get; set; }
+
+
+
+    }
+}
