f1ecd673262e1590a497488f707bc85dca55a6d2
aau19 <aau19@DESKTOP-M0K5TMI>
2019-11-11 10:25:09 -0800

    Reading and Adding in Identity Razor Pages to Pages folder

--- Bookmi/Models/ProviderAccount.cs
+++ Bookmi/Models/ProviderAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,17 +15,24 @@ namespace Bookmi.Models
 
         public string ServiceProvided { get; set; }
 
+        [Required]
         public string UserName { get; set; }
 
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
         public DateTime DateOfBirth { get; set; }
+
 
         public string AboutProvider { get; set; }
 
+        [Required]
         public string EmailAddress { get; set; }
+
 
         public string PhoneNumber { get; set; }
 
